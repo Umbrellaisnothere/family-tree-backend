39bda853af6798b8528d8051b9406c7d71625832
Keith Murimi <keith.murimi@student.moringaschool.com>
2025-05-09 22:43:12 +0300

    Enhanced the addPerson function to include gender, relationship, and parent_Id parameters

--- addPerson.js
+++ addPerson.js
@@ -1,8 +1,8 @@
 const db = require('./database');
 
-function addPerson(name, birthDate, deathDate, image) {
-    const query = `INSERT INTO Person (name, birthDate, deathDate, image) VALUES (?, ?, ?, ?)`;
-    db.run(query, [name, birthDate, deathDate, image], function(err) {
+function addPerson(name, birthDate, deathDate, image, gender, relationship, parent_Id, callback) {
+    const query = `INSERT INTO Person (name, birthDate, deathDate, image, gender, relationship, parent_Id) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+    db.run(query, [name, birthDate, deathDate, image, gender, relationship, parent_Id], function(err) {
         if (err) {
             console.error('Error adding person: ' + err.message);
         } else {
@@ -25,9 +25,13 @@ function addRelationship(personId1, personId2, type) {
 }
 
 // An Example usage
-addPerson('John Doe', '1980-01-01', null, 'https://picsum.photos/80', (johnId) => {
-    addPerson('Jane Doe', '1985-05-15', null, 'https://picsum.photos/81', (janeId) => {
+addPerson('John Doe', '1980-01-01', null, 'https://picsum.photos/80', 'male', null, null, (johnId) => {
+    addPerson('Jane Doe', '1985-05-15', null, 'https://picsum.photos/81', 'female', null, null, (janeId) => {
         addRelationship(johnId, janeId, 'spouse');
+
+        addPerson('Little Doe', '2010-01-01', null, null, 'female', null, johnId, (childId) => {
+            console.log('Child added:', childId);
+            addRelationship(johnId, childId, 'parent');
+        });
     });
 });
-// addRelationship(1, 2, 'spouse');
